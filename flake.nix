34c11e55676a687cc9f87358ddae950ae82a6d09
ozwaldorf <self@ossian.dev>
2024-07-18 18:01:15 -0400

    feat(nix): add garnix cache

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
   outputs =
     { self, nixpkgs }:
     let
