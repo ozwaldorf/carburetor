2e74421de792c561237e12dbe7dfc60c6007b03e
ozwaldorf <self@ossian.dev>
2024-07-18 17:44:57 -0400

    chore: minor fixes

--- nix/home/wezterm.nix
+++ nix/home/wezterm.nix
@@ -2,6 +2,8 @@
 {
   options.carburetor.wezterm.enable = lib.mkEnableOption "installing carburetor themes to wezterm";
   config = lib.mkIf config.carburetor.wezterm.enable {
-    xdg.configFile."wezterm/colors".source = ../../src/wezterm;
+    xdg.configFile."wezterm/colors/carburetor.toml".source = ../../src/wezterm/carburetor.toml;
+    xdg.configFile."wezterm/colors/carburetor-cool.toml".source = ../../src/wezterm/carburetor-warm.toml;
+    xdg.configFile."wezterm/colors/carburetor-warm.toml".source = ../../src/wezterm/carburetor-warm.toml;
   };
 }
--- nix/home/zed.nix
+++ nix/home/zed.nix
@@ -7,6 +7,6 @@
 {
   options.carburetor.zed.enable = lib.mkEnableOption "installing carburetor themes to zed";
   config = lib.mkIf config.carburetor.zed.enable {
-    xdg.configFile."zed".source = pkgs.carburetor-zed;
+    xdg.configFile."zed/themes".source = pkgs.carburetor-zed + "/themes";
   };
 }
