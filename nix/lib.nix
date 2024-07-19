8a301bfa44532d5e831a6f43fbb2ead5b0858dce
ozwaldorf <self@ossian.dev>
2024-07-19 00:16:45 -0400

    docs(nix): add examples to lib.nix

--- nix/lib.nix
+++ nix/lib.nix
@@ -1,4 +1,35 @@
 {
+  # Create a custom overlay for a theme.
+  #
+  # ```nix
+  # pkgs = import nixpkgs {
+  #   inherit system;
+  #   overlays = [
+  #     (inputs.carburetor.lib.mkCustomThemeOverlay {
+  #       name = "foo-theme";
+  #       variantNames = {
+  #         mocha = "darker";
+  #         macchiato = "dark";
+  #         frappe = "medium";
+  #         latte = "light";
+  #       };
+  #       whiskersJson = ./path/to/whiskers.json;
+  #     })
+  #   ];
+  # }
+  # ```
   mkCustomThemeOverlay = args: (import ./pkgs args);
-  mkCustomHomeManagerModule = args: (import ./home/default.nix) args;
+
+  # Create a custom home manager module to install themes with.
+  # Must be paired with `mkCustomThemeOverlay` for the packages to be available.
+  #
+  # ```nix
+  # imports = [ (inputs.carburetor.lib.mkCustomHomeManagerModule "foo-theme") ];
+  #
+  # foo-theme = {
+  #   wezterm.enable = true;
+  #   zed.enable = true;
+  # };
+  # ```
+  mkCustomHomeManagerModule = name: (import ./home/default.nix) name;
 }
