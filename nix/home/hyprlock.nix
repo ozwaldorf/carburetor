a41c82e2b1f0a238a8a821421d006de2b3dd084f
ozwaldorf <self@ossian.dev>
2024-07-20 12:51:58 -0400

    fix(home): conflicting xdg.configFile entry between hypr{land,lock}

--- nix/home/hyprlock.nix
+++ nix/home/hyprlock.nix
@@ -8,9 +8,8 @@
 {
   options.${name}.themes.hyprlock.enable = lib.mkEnableOption "installing ${name} for hyprlock";
   config = lib.mkIf config.${name}.themes.hyprlock.enable {
-    xdg.configFile."hypr" = {
-      source = pkgs.${name}.hyprlock.override { inherit (config.${name}.config) variant accent; };
-      recursive = true;
-    };
+    xdg.configFile."hypr/hyprlock.conf".source =
+      pkgs.${name}.hyprlock.override { inherit (config.${name}.config) variant accent; }
+      + "/hyprlock.conf";
   };
 }
