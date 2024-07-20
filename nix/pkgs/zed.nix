1e6e89c51fe43f822c4a350e08f96828c08860bc
ozwaldorf <self@ossian.dev>
2024-07-20 19:33:15 -0400

    tweak(zed): set current line opacity to 0.02

--- nix/pkgs/zed.nix
+++ nix/pkgs/zed.nix
@@ -8,4 +8,15 @@ pkgs."${name}".tools.mkWhiskersDerivation {
     rev = "6ac674645711195d90f02876e74c79256848ff22";
     hash = "sha256-jOHCO4wBqm2oNwVSJ3S9npKE5Gsen6ai9TZd9IMuevc=";
   };
+  patches = [
+    (pkgs.writeText "current_line.patch" ''
+      --- a/zed.tera
+      +++ b/zed.tera
+      @@ -79,3 +79,3 @@ whiskers:
+                       "editor.subheader.background": "#{{c.mantle.hex}}",
+      -                "editor.active_line.background": "#{{ c.text | mod(opacity=0.05) | get(key="hex") }}",
+      +                "editor.active_line.background": "#{{ c.text | mod(opacity=0.02) | get(key="hex") }}",
+                       "editor.highlighted_line.background": null,
+    '')
+  ];
 }
