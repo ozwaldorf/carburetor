d8e74e37e0398cd02604d094c5bd2e446d49125a
ozwaldorf <self@ossian.dev>
2026-03-17 15:28:51 -0400

    zed: 0.2.11 -> 0.2.24

--- nix/pkgs/zed.nix
+++ nix/pkgs/zed.nix
@@ -2,12 +2,12 @@
 { pkgs, fetchFromGitHub, ... }:
 pkgs."${name}".tools.mkWhiskersDerivation {
   pname = "zed";
-  version = "0.2.11-unstable-2024-09-09";
+  version = "0.2.24-unstable-2025-11-10";
   src = fetchFromGitHub {
     owner = "catppuccin";
     repo = "zed";
-    rev = "ba6aa143158585ca92294cf994ac8cfbe74b56c7";
-    hash = "sha256-T+f/sM5BYz+hLc3RVVhn38C+Sfr0XHtsRWzTd5JPVTI=";
+    rev = "471eb7e5a432f694c272ee8b17ba56c05c6a99b0";
+    hash = "sha256-/sIx1WoYNsP+D/kla0x09e0z6kqpLyskp1f9eH1aNAU=";
   };
   patches = [
     (pkgs.writeText "current_line.patch" ''
