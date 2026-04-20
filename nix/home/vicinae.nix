94cf878e5178d150455b032fc51dadaddaae7c82
Keegan Conlee <dacid44@gmail.com>
2026-04-20 12:15:05 -0500

    fix: update vicinae theme to new format and remove the implicit dependency on the vicinae HM module

--- nix/home/vicinae.nix
+++ nix/home/vicinae.nix
@@ -6,30 +6,42 @@
   ...
 }:
 let
-  cfg = config.${name}.themes.foot;
+  cfg = config.${name}.themes.vicinae;
   options = config.${name}.config;
 in
 {
   options.${name}.themes.vicinae.enable = lib.mkEnableOption "installing ${name} for vicinae";
-  config = lib.mkIf cfg.enable {
-    services.vicinae.themes.carburetor = {
-      version = "1.0";
-      appearance = "dark";
-      # icon = /path/to/icon.png;
-      name = "carburetor";
-      description = "High contrast theme based on IBM Carbon";
-      palette = {
-        background = "#161616";
-        foreground = "#f4f4f4";
-        blue = "#4589ff";
-        green = "#42be65";
-        magenta = "#ff7eb6";
-        orange = "#ff832b";
-        purple = "#be95ff";
-        red = "#fa4d56";
-        yellow = "#fddc69";
-        cyan = "#3ddbd9";
-      };
-    };
-  };
+  config = lib.mkIf cfg.enable (
+    lib.genAttrs [ "services" "programs" ] (
+      source:
+      lib.optionalAttrs (config.${source} ? vicinae) {
+        vicinae.themes.carburetor = {
+          meta = {
+            version = "1.0";
+            name = "carburetor";
+            description = "High contrast theme based on IBM Carbon";
+            variant = "dark";
+            # icon = /path/to/icon.png;
+            inherits = "vicinae-dark";
+          };
+          colors = {
+            core = {
+              background = "#161616";
+              foreground = "#f4f4f4";
+            };
+            accents = {
+              blue = "#4589ff";
+              green = "#42be65";
+              magenta = "#ff7eb6";
+              orange = "#ff832b";
+              purple = "#be95ff";
+              red = "#fa4d56";
+              yellow = "#fddc69";
+              cyan = "#3ddbd9";
+            };
+          };
+        };
+      }
+    )
+  );
 }
