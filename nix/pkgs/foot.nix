8590518a97569c61621b51c3b664d63b0d80b286
ozwaldorf <self@ossian.dev>
2026-08-03 16:02:29 -0400

    foot: 0-unstable-2025-07-20 -> 0-unstable-2026-04-25

--- nix/pkgs/foot.nix
+++ nix/pkgs/foot.nix
@@ -2,11 +2,11 @@
 { pkgs, fetchFromGitHub, ... }:
 pkgs."${name}".tools.mkWhiskersDerivation {
   pname = "foot";
-  version = "0-unstable-2025-07-20";
+  version = "0-unstable-2026-04-25";
   src = fetchFromGitHub {
     owner = "catppuccin";
     repo = "foot";
-    rev = "8d263e0e6b58a6b9ea507f71e4dbf6870aaf8507";
-    hash = "sha256-bpGVDESE6Pr7kaFgfAWJ/5KC9mRPlv2ciYwRr6jcIKs=";
+    rev = "99384a83ee9246cd0a38aeee07d8300367724602";
+    hash = "sha256-lFa5EpoLkrZcC80YDHyVOTnwYOCNybznlD80NkgVLYs=";
   };
 }
